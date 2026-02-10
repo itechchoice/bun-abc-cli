a4c3b02ec5dcee50858512c2ffe065fc16603533
hejing <jing.he@itechchoice.com>
2026-02-10 15:57:19 +0800

    style: adjust greeting banner's gradient colors, font, and height.

--- src/ui/GreetingBanner.tsx
+++ src/ui/GreetingBanner.tsx
@@ -1,23 +1,23 @@
 import { TextAttributes } from "@opentui/core";
 
-const GRADIENT_COLORS = ["#62A7FF", "#72B9F4", "#86CBE1", "#9ADBBE"];
+const GRADIENT_COLORS = ["#4A9EF5", "#5BB8E8", "#6DD0D0", "#86DEB0", "#A8E89C"];
 
 export function GreetingBanner() {
   return (
     <box
       flexDirection="column"
       justifyContent="space-between"
-      height={11}
+      height={8}
       paddingLeft={1}
       paddingRight={1}
       paddingTop={1}
       paddingBottom={1}
     >
-      <box flexDirection="row" gap={1} alignItems="flex-start" height={8}>
+      <box flexDirection="row" gap={1} alignItems="flex-start" height={5}>
         <text fg="#C887FF" attributes={TextAttributes.BOLD}>
           &gt;
         </text>
-        <ascii-font font="huge" text="ABC CLI" color={GRADIENT_COLORS} />
+        <ascii-font font="block" text="ABC CLI" color={GRADIENT_COLORS} />
       </box>
       <text attributes={TextAttributes.DIM}>abc-cli preview | OpenTUI + React + TypeScript + Bun</text>
     </box>
